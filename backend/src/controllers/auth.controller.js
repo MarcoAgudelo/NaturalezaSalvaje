3ad9a967b85677d3ebb080f8c778352abe9fc850
Marco Antonio Agudelo Corrales <marcoantonioagudelo805@gmail.com>
2026-04-08 23:54:48 -0500

    POST /api/auth/login. Inicio de sesión normal.

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -54,6 +54,73 @@ const crearUsuario = async (req, res) => {
    
 };
 
+const loginUsuario = async (req, res) => {
+    try{
+        const { email, password } = req.body;
+
+        if (!email || !password) {
+            return res.status(400).json({
+                message: "Todos los campos son obligatorios",
+            });
+        }
+
+        const emailNormalizado = email.trim().toLowerCase();
+        const passwordLimpia = password.trim();
+
+        if (!emailNormalizado || !passwordLimpia) {
+            return res.status(400).json({
+                message: "Todos los campos son obligatorios",
+            });
+        }
+
+        const existeUsuario = await authModel.obtenerUsuarioPorEmail(emailNormalizado);
+
+        if (!existeUsuario) {
+            return res.status(401).json({
+                message: "Usuario no encontrado",
+            });
+        }
+
+        const passwordValida = await bcrypt.compare(passwordLimpia, existeUsuario.password);
+
+        if (!passwordValida) {
+            return res.status(401).json({
+                message: "Usuario no encontrado",
+            });
+        }
+
+        const token = jwt.sign(
+            {
+                id: existeUsuario.id,
+                email: existeUsuario.email,
+            },
+            process.env.JWT_SECRET,
+            {
+                expiresIn: "1h",
+            }
+        );
+
+        return res.status(200).json({
+            message: "Login exitoso",
+            token,
+            user: {
+                id: existeUsuario.id,
+                nombres: existeUsuario.nombres,
+                apellidos: existeUsuario.apellidos,
+                email: existeUsuario.email,
+                rol: existeUsuario.rol
+            },
+        });
+
+    }catch(error){
+        console.error("Error al iniciar sesion", error.message);
+        return res.status(500).json({
+            message: "Error internio del servidor",
+        });
+    }
+};
+
 module.exports = {
-    crearUsuario
+    crearUsuario,
+    loginUsuario
 };
--- backend/src/routes/auth.routes.js
+++ backend/src/routes/auth.routes.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const authController = require("../controllers/auth.controller");
 
 router.post("/registro", authController.crearUsuario);
+router.post("/login", authController.loginUsuario);
 
 
 module.exports = router;
